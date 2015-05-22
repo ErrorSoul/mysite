eb54517e98a6ce4cb75bcf59c6573b835fef3995
Abu Sultanov <neurofag@gmail.com>
2015-05-22 11:43:12 +0300

    fix redactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RedactorRails::Engine => '/redactor_rails'
+  #mount RedactorRails::Engine => '/redactor_rails'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
